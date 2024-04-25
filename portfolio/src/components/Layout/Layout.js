893412dc7139e5c6e298c577f00bdc01aa38dac8
valenwhite <valenwhite@users.noreply.github.com>
2024-04-25 19:03:47 +1000

    Corrected Home page CSS

--- portfolio/src/components/Layout/Layout.js
+++ portfolio/src/components/Layout/Layout.js
@@ -1,7 +1,6 @@
 import styles from './Layout.module.css';
 import { SideBar } from '../SideBar/SideBar';
 import { Home } from '../Home/Home';
-import '../../App.css'; 
 
 
 export const Layout = () => {
